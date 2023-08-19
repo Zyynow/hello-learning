439a5faeef6159dfda5518d75ed9bd77e08370dd
Zyynow <128598666+Zyynow@users.noreply.github.com>
2023-08-19 10:27:23 +0000

    5

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -17,58 +17,58 @@ module.exports = {
         ]
       }
     ],
-    sidebar: {
-      '/data-structures/': [
-        '',     /* /foo/ */
-        'Arrays',  /* /foo/one.html */
-        'Linked Lists',   /* /foo/two.html */
-        'Queues',
-        'Stacks', 
-        'Trees',
-        'Hash Tables',
-        'Graphs',
-      ],
-    }
+    // sidebar: {
+    //   '/data-structures/': [
+    //     '',     /* /foo/ */
+    //     'Arrays',  /* /foo/one.html */
+    //     'Linked Lists',   /* /foo/two.html */
+    //     'Queues',
+    //     'Stacks', 
+    //     'Trees',
+    //     'Hash Tables',
+    //     'Graphs',
+    //   ],
+    // }
+    sideBar:[
+      {
+        title: 'Data Structures',   // 必要的
+        path: '/data-structures/Data Structures.md',   
+        // children: [ 
+        //   {
+        //     title: 'Arrays',
+        //     path: '/data-structures/Arrays.md',
+        //   },
+        //   {
+        //     title: 'Linked Lists',
+        //     path: '/data-structures/Linked List.md',
+        //   },
+        //   {
+        //     title: 'Queues',
+        //     path: '/data-structures/Queues.md',
+        //   },
+        //   {
+        //     title: 'Stacks',
+        //     path: '/data-structures/Stacks.md',
+        //   },
+        //   {
+        //     title: 'Trees',
+        //     path: '/data-structures/Trees.md',
+        //   },
+        //   {
+        //     title: 'Hash Tables',
+        //     path: '/data-structures/Hash Tables.md',
+        //   },
+        //   {
+        //     title: 'Graphs',
+        //     path: 'docs/data-structures/Graphs.md',
+        //   },
 
-      // {
-      //   title: 'Data Structures',   // 必要的
-      //   path: '/data-structures/Data Structures.md',   
-      //   children: [ 
-      //     {
-      //       title: 'Arrays',
-      //       path: '/data-structures/Arrays.md',
-      //     },
-      //     {
-      //       title: 'Linked Lists',
-      //       path: '/data-structures/Linked List.md',
-      //     },
-      //     {
-      //       title: 'Queues',
-      //       path: '/data-structures/Queues.md',
-      //     },
-      //     {
-      //       title: 'Stacks',
-      //       path: '/data-structures/Stacks.md',
-      //     },
-      //     {
-      //       title: 'Trees',
-      //       path: '/data-structures/Trees.md',
-      //     },
-      //     {
-      //       title: 'Hash Tables',
-      //       path: '/data-structures/Hash Tables.md',
-      //     },
-      //     {
-      //       title: 'Graphs',
-      //       path: 'docs/data-structures/Graphs.md',
-      //     },
-
-      //    ],
-      // },
-      // {
-      //   title: 'Group 2',
-      // },
-    
-  }
+        //  ],
+      },
+      {
+        title: 'Group 2',
+      },
+    ],
+  },
   
 }
