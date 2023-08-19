69307710d14fa4d526137d69e70145a9ad760c84
Zyynow <128598666+Zyynow@users.noreply.github.com>
2023-08-19 09:54:41 +0000

    Add new md

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -21,6 +21,37 @@ module.exports = {
       {
         title: 'Data Structures',   // 必要的
         path: '/data-structures/Data Structures.md',   
+        children: [ 
+          {
+            text: 'Arrays',
+            link: '/data-structures/Arrays.md',
+          },
+          {
+            text: 'Linked Lists',
+            link: '/data-structures/Linked List.md',
+          },
+          {
+            text: 'Queue',
+            link: 'docs/data-structures/Queues.md',
+          },
+          {
+            text: 'Stacks',
+            link: 'docs/data-structures/Stacks.md',
+          },
+          {
+            text: 'Trees',
+            link: 'docs/data-structures/Trees.md',
+          },
+          {
+            text: 'Hash Tables',
+            link: 'docs/data-structures/Hash Tables.md',
+          },
+          {
+            text: 'Graphs',
+            link: 'docs/data-structures/Graphs.md',
+          },
+
+         ],
       },
       {
         title: 'Group 2',
